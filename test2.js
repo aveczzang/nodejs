fb43f052e716e132ca87fa08f20de5771fefd7b7
A310-37 <avec@dreamwiz.com>
2018-07-28 16:18:51 +0900

    modify

--- test2.js
+++ test2.js
@@ -1,9 +1,9 @@
 let date = new Date();
 
 if(date.getHours() < 12) {
-    console.log("오전")
+    console.log("오전입니다.")
 }
 
 if(date.getHours() >= 12) {
-    console.log("오후")
+    console.log("오후입니다.")
 }
